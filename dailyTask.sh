17901a0e75cae813670f9f84b16deb6682dc79e5
Jason Qiu <jason.blank.qiu@gmail.com>
2021-10-04 12:55:13 -0500

    Minor fixes

--- dailyTask.sh
+++ dailyTask.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-taskFile='./tasks.txt'
-todaysTaskFile='./TodaysTask.txt'
+taskFile=$HOME'/scripts/tasks.txt'
+todaysTaskFile=$HOME'/scripts/TodaysTask.txt'
+numTasks=1
 
 # Edit if e or edit argument
 if [[ $1 = "e" ]] || [[ $1 = "edit" ]] 
@@ -9,45 +10,56 @@ then
     $VISUAL "$taskFile"
     exit
 else 
-    if [[ $1 = "t" ]] 
+    # number of tasks to generate
+    if [[ $1 =~ ^[0-9]+$ ]]
     then
-        map=(0,0,0,0,0,0,0,0,0,0)
-        for i in {1..1000}
-        do
-            RANDOM=10#$(date +%s%N | cut -b10-17)
-            rand=$(($RANDOM % 10))
-            map[$rand]=$((${map[$rand]}+1))
-        done
-        echo ${map[*]}
-        exit
+        numTasks=$1
+    else
+        if [[ $1 = "t" ]] 
+        then
+            map=(0,0,0,0,0,0,0,0,0,0)
+            for i in {1..1000}
+            do
+                RANDOM=10#$(date +%s%N | cut -b10-17)
+                rand=$(($RANDOM % 10))
+                map[$rand]=$((${map[$rand]}+1))
+            done
+            echo ${map[*]}
+            exit
+        fi
     fi
 fi
 
-# Print cached task if already generated for today
-if [ 10#$(find "$todaysTaskFile" -printf '%Cd' 2>/dev/null) == 10#$(date +%d) ] 
+# Print cached task if already generated for today and same number of tasks
+if [ 10#$(find "$todaysTaskFile" -printf '%Cd' 2>/dev/null) == 10#$(date +%d) ] && [ $(cat "$todaysTaskFile" | wc -l) == $numTasks ]
 then
-    echo $(cat "$todaysTaskFile")
+    cat "$todaysTaskFile"
     exit
 fi
 
 # Otherwise, generate a new task file
 # Remove lines beginning with '#' and blank lines
+>| "$todaysTaskFile"
 tasks="$(awk '!/^$|^[[:space:]]*#/' "$taskFile")"
 numLines=$(echo "$tasks" | wc -l)
-# Seed a random number and pick a number less than numLines 
-RANDOM=10#$(date +%s%N | cut -b10-17)
-chosenTask=$(($RANDOM % $numLines))
 
-i=0
-IFS=$'\n'
-for line in $tasks
+for numChosenTasks in $(seq 1 $numTasks)
 do
-    if [ $i == $chosenTask ]
-    then
-        # Print and cache
-        echo "$line"
-        echo "$line" > "$todaysTaskFile"
-        break
-    fi
-    i=$(($i+1))
+    # Seed a random number and pick a number less than numLines 
+    RANDOM=10#$(date +%s%N | cut -b10-17)
+    chosenTask=$(($RANDOM % $numLines))
+
+    i=0
+    IFS=$'\n'
+    for line in $tasks
+    do
+        if [ $i == $chosenTask ]
+        then
+            # Print and cache
+            echo "$line"
+            echo "$line" >> "$todaysTaskFile"
+            break
+        fi
+        i=$(($i+1))
+    done
 done
---
+++ notes.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+notesDir='/home/Jason/notes/'
+
+if [[ $# > 0 ]] 
+then
+    if [[ $# == 1 ]] && [[ $1 =~ ^[0-9]+$ ]]
+    then
+        # assume number argument is notes from $1 days ago
+        date=$(( $(date +%Y%m%d) - $1))
+        noteFile="$notesDir"daily/"$date"
+    else
+        # if not just one number for daysAgo, then assume file path, where
+        # last argument is file name
+        noteFile="$notesDir"
+        for path in ${@:1:$(($#-1))}
+        do
+            noteFile="$noteFile""$path"/
+        done
+
+        # make path if it doesn't exist
+        if ! [[ -d "$noteFile" ]]
+        then
+            mkdir -p "$noteFile"
+        fi
+
+        noteFile="$noteFile""${@: -1}"
+    fi
+else
+    # default behavior, pull up today's note file
+    date=$(date +%Y%m%d)
+    noteFile="$notesDir"daily/"$date"
+fi
+
+
+#if being piped, then append stdout to noteFile
+if (ls -l /proc/$$/fd/0 | grep -qv dev )
+then
+    touch "$noteFile"
+    cat - >> "$noteFile"
+    exit
+fi
+
+#if output being redirected, print notes 
+if ( ls -l /proc/$$/fd/1 | grep -qv dev ) 
+then
+    cat "$noteFile"
+    exit
+fi
+
+# if normal execution, then edit notefile 
+$VISUAL "$noteFile"
