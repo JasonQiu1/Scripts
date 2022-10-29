9eabbf08b56a9321897f0754a4ac206617b0e2d3
Jason Qiu <jason.blank.qiu@gmail.com>
2022-10-29 16:39:32 +0800

    Fixed date bug on the boundaries of months. Added templated date functionaliy.

--- notes.sh
+++ notes.sh
@@ -2,13 +2,36 @@
 
 notesDir='/home/Jason/notes/'
 fileExtension='.md'
+today="$(date +%Y%m%d)"
+date="$today"
+
+fdate() {
+    date=$(date -d "$1" +%Y%m%d)
+}
+
+addPath() {
+    if [ "${1:0:1}" = "{" ]
+    then
+        # templated date
+        if [ "${1:1:1}" != "}" ]
+        then
+            fdate "${1:1:$((${#1} - 2))} days ago"
+        else
+            date="$today"
+        fi
+        noteFile="$noteFile$date"
+    else
+        # regular text
+        noteFile="$noteFile$1"
+    fi
+}
 
 if [[ $# > 0 ]] 
 then
-    if [[ $# == 1 ]] && [[ $1 =~ ^[0-9]+$ ]]
+    if [[ $# == 1 ]] && [[ $1 =~ ^[0-9]+|^-[0-9]+$ ]]
     then
         # assume number argument is notes from $1 days ago
-        date=$(( $(date +%Y%m%d) - $1))
+        fdate "$1 days ago"
         noteFile="$notesDir"daily/"$date$fileExtension"
     else
         # if not just one number for daysAgo, then assume file path, where
@@ -16,7 +39,8 @@ then
         noteFile="$notesDir"
         for path in ${@:1:$(($#-1))}
         do
-            noteFile="$noteFile""$path"/
+            addPath "$path"
+            noteFile="$noteFile"/
         done
 
         # make path if it doesn't exist
@@ -25,16 +49,17 @@ then
             mkdir -p "$noteFile"
         fi
 
-        if [[ "${@: -1}" =~ [a-zA-Z0-9]+\.[a-zA-Z0-9]+ ]]
+        # add the file name to the end
+        addPath "${@: -1}"
+
+        # add the default file extension if the filename did not include it
+        if ! [[ "${@: -1}" =~ [a-zA-Z0-9]+\.[a-zA-Z0-9]+ ]]
         then
-            noteFile="$noteFile${@: -1}"
-        else
-            noteFile="$noteFile${@: -1}$fileExtension"
+            noteFile="$noteFile$fileExtension"
         fi
     fi
 else
     # default behavior, pull up today's note file
-    date=$(date +%Y%m%d)
     noteFile="$notesDir"daily/"$date$fileExtension"
 fi
 
